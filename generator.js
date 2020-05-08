f6e0665356af6014a63714e9efc07e16e06bb972
Aresiel <love.dittlau@gmail.com>
2020-05-08 14:25:16 +0200

    The thing

---
+++ generator.js
@@ -0,0 +1,9 @@
+function generate() {
+	let charArr = ['Æ', 'Ø', 'Þ', 'ß', 'ð']
+	let name = ""
+	name += (Math.floor(Math.random()*26)+10).toString(36).toUpperCase()
+	name += " " + charArr[Math.floor(Math.random()*charArr.length)]
+	name += " " + (Math.floor(Math.random()*26)+10).toString(36).toUpperCase() + "-" + Math.floor(Math.random()*89+10)
+
+	document.getElementById('name').value = name
+}
